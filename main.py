bf93be8516ddc078107de16d0da5208f918d32ba
Luis Andres <luisandrevelazquez.d@gmail.com>
2023-05-01 19:31:56 +0200

    Created SQL database, modules in package app, minor changes in main.py

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+# Library imports
 import requests
-import creds
 import json
 from requests.auth import HTTPBasicAuth
 import pandas as pd
 from cachetools import cached, TTLCache
+
+# Modules from 'app' package
+import app.creds as creds
+from app.database import ApiDatabase
+from app.houseparser import HouseDataParser
 
 cache = TTLCache(maxsize=100, ttl=300)  # create a cache with a maximum of 100 entries and a time-to-live (TTL) of 300 seconds (5 minutes)
 
@@ -20,12 +25,21 @@ def get_auth(key, secret, oathurl):
     Gets token from API_KEY provided by idealista https://developers.idealista.com/access-request
     """
     payload = {"grant_type": "client_credentials"}
+    authentication = HTTPBasicAuth(key, secret)
     r = requests.post(oathurl,
-                    auth=HTTPBasicAuth(key, secret),
+                    auth=authentication,
                     data=payload)
     # If the request fails, we print an error message.
     if r.status_code == 200:
         print(f"API credentials successfully retrieved from {oathurl}")
+        
+        # Initiate a database connection to keep the request in memory
+        api_db = ApiDatabase(api_key=authentication,db_path='output/api_data.db')
+        api_db.connect()
+        data = api_db.make_request(oathurl,r)
+        print(data)
+        # Close the database connection
+        api_db.close()
         return r.text
     else:
         raise Exception(f"API request failed with status code {r.status_code}")
@@ -43,34 +57,23 @@ def create_session(token):
     return s 
 
 
-def get_df(data_json):
-    return pd.DataFrame.from_dict(data_json['elementList'])
-
-def parse_df(df):
-    return df[['price', 'size', 'rooms', 'bathrooms', 'district', 'neighborhood']].astype({"price":int,"size":int})
-
-def get_csv(df):
-    return df.to_csv('output/houses.csv', index=False)
-
-def get_excel(df):
-    return df.to_excel('output/houses.xlsx', index=False)
-
 def main():
     # Error handling
     try:
         token = json.loads(get_auth(creds.API_KEY, creds.API_SECRET, creds.OATH_URL))
         sess = create_session(token['access_token'])
         resp = sess.post(creds.BASE_URL + "?center={0}&operation={1}&propertyType={2}&country={3}&maxItems=50&distance={4}".format(loc,op,ptype,country,dist))
+        search_response = json.loads(resp.text)
+        house_parser = HouseDataParser(search_response)
+        house_parser.parse_data()
+        house_parser.to_csv()
+        house_parser.to_excel()
+
     except requests.exceptions.HTTPError as error:
         print(f"HTTP error occurred: {error}")
     except Exception as error:
         print(f"Other error occurred: {error}")
-        
-    search_response = json.loads(resp.text)
-    dataframe = get_df(search_response)
-    df_final = parse_df(dataframe)
-    get_csv(df_final)
-    get_excel(df_final)
+
             
 if __name__ == "__main__":
     main()
